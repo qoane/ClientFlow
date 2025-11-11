34703f6e4b2d3abc687f41287d418e426966a6dc
Qoane Seitlheko <sqoane@gmail.com>
2025-11-11 08:46:24 +0200

    Require admin auth when managing surveys

---
+++ ClientFlow.Infrastructure/Migrations/20251111064116_EnsureSettingsJsonColumns.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnsureSettingsJsonColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF COL_LENGTH('dbo.Questions', 'SettingsJson') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Questions]
+    ADD [SettingsJson] nvarchar(max) NULL;
+END
+");
+
+            migrationBuilder.Sql(@"
+IF COL_LENGTH('dbo.Sections', 'SettingsJson') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Sections]
+    ADD [SettingsJson] nvarchar(max) NULL;
+END
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF COL_LENGTH('dbo.Questions', 'SettingsJson') IS NOT NULL
+BEGIN
+    ALTER TABLE [dbo].[Questions]
+    DROP COLUMN [SettingsJson];
+END
+");
+
+            migrationBuilder.Sql(@"
+IF COL_LENGTH('dbo.Sections', 'SettingsJson') IS NOT NULL
+BEGIN
+    ALTER TABLE [dbo].[Sections]
+    DROP COLUMN [SettingsJson];
+END
+");
+        }
+    }
+}
